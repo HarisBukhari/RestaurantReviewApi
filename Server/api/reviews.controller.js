8fc7d1a2bf57408769ecb050089eb8b696d58097
Syed Haris Bukhari <syedharishussain86@gmail.com>
2022-07-21 06:34:29 -0700

    finishing Api

--- Server/api/reviews.controller.js
+++ Server/api/reviews.controller.js
@@ -3,7 +3,6 @@ export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
       const restaurantId = req.body.restaurant_id
-      console.log(restaurantId)
       const review = req.body.text
       const userInfo = {
         name: req.body.name,
@@ -57,7 +56,6 @@ export default class ReviewsController {
     try {
       const reviewId = req.query.id
       const userId = req.body.user_id
-      console.log(reviewId)
       const reviewResponse = await ReviewsDAO.deleteReview(
         reviewId,
         userId,
